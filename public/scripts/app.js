93d91e0bd9a6ae35fe8936b6aa7bf99e4a708ec9
boxton90 <daniel.rabal@dnata.com>
2018-11-29 12:35:19 +0400

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,54 @@
 
 console.log('App.js is loaded!');
 
+var app = {
+    title: 'Indecision',
+    subtitle: 'Put your life in the hand of a computer'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
 );
+
+var user = {
+    name: 'Daniel',
+    age: 28,
+    location: 'Madrid'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 console.log('App.js is loaded!')
 
-let template = <h1>Indecision App</h1>
+const app = {
+    title: 'Indecision',
+    subtitle: 'Put your life in the hand of a computer'
+}
+
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+)
+
+const user = {
+    name: 'Daniel',
+    age: 28,
+    location: 'Madrid'
+}
+
+const templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+)
+
 let appRoot = document.getElementById('app')
 
 ReactDOM.render(template,appRoot)
