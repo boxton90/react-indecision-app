3a4a4896150a6a660d17f7ae4c79646005e798bd
boxton90 <boxton.da@gmail.com>
2018-12-03 19:12:21 +0400

    Arrays in JSX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log('App.js is loaded!')
 
 const app = {
-    title: 'Indecision',
+    title: 'Indecision App',
     subtitle: 'Put your life in the hand of a computer',
     options: []
 }
@@ -29,6 +29,9 @@ const render = () => {
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
             <p>{app.options.length}</p>
             <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                {app.options.map( (option,index) => <li key={index}>{option}</li>)}
+            </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option"></input>
                 <button>Add Option</button>
