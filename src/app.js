617625b2bed0166a3340c34854225b4781620ceb
boxton90 <boxton.da@gmail.com>
2018-12-20 12:20:33 +0400

    Default Exports

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-import { isAdult, canDrink } from './person.js'
+import isSenior, { isAdult, canDrink } from './person.js'
+import subtract, { square, add } from './utils.js'
+
+console.log(subtract(100, 10))
 
 console.log('App.js is running!')
 console.log(isAdult(25))
 console.log(canDrink(12))
+console.log(isSenior(65))
