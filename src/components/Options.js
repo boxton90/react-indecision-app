402f482c9deaa9e1e1641cedfeab3363eb3c01df
boxton90 <boxton.da@gmail.com>
2018-12-29 13:01:55 +0100

    Styling Option Item

--- src/components/Options.js
+++ src/components/Options.js
@@ -17,6 +17,7 @@ const Options = (props) => (
             <Option
                 key={index}
                 optionText={option}
+                count={index + 1}
                 handleDeleteOption={props.handleDeleteOption} />
         )}
     </div>
